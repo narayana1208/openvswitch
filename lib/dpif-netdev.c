6d4277e3e4237450c1b0ebad73c4fdd025afdc48
Lorand Jakab <lojakab@cisco.com>
2014-04-03 21:52:21 +0300

    userspace: add support for pop_eth and push_eth actions
    These actions will allow L2->L3 and L3->L2 switching, and are supposed
    to be added to flows installed in the datapath transparently by
    ovs-vswitchd.
    
    Signed-off-by: Lorand Jakab <lojakab@cisco.com>

--- lib/dpif-netdev.c
+++ lib/dpif-netdev.c
@@ -2177,6 +2177,8 @@ dp_execute_cb(void *aux_, struct ofpbuf *packet,
         }
         break;
 
+    case OVS_ACTION_ATTR_PUSH_ETH:
+    case OVS_ACTION_ATTR_POP_ETH:
     case OVS_ACTION_ATTR_PUSH_VLAN:
     case OVS_ACTION_ATTR_POP_VLAN:
     case OVS_ACTION_ATTR_PUSH_MPLS:
